203a0cab4f57c3893e570c01637c6d5f0e927323
Eze Daniel <dan4allu93@gmail.com>
2018-06-17 21:47:58 +0100

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
+import { routes } from './shared/routes/routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/user/login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/assets/js/navScroll.js
@@ -0,0 +1,32 @@
+var lastScrollTop = 0;
+var delta = 20;
+var navbarHeight = $('.nav').outerHeight();
+var didScroll;
+
+$(window).scroll(function(e) {
+    didScroll = true;
+});
+
+setInterval(() => {
+  if(didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+// Do some manipulation when user scrolls
+function hasScrolled() {
+  var st = $(this).scrollTop();
+
+  if(Math.abs(lastScrollTop - st) <= delta)
+    return;
+
+  if(st > lastScrollTop && st > navbarHeight) {
+    $('.nav').addClass('nav-up')
+  }else {
+    if (st + $(window).height() < $(document).height()) {
+      $('.nav').removeClass('nav-up')
+    }
+  }
+  lastScrollTop = st;
+}
